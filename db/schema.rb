0b1c72929148eb6aa494bf52462542d8fd71357e
Kat Gifford <katgiff2007@gmail.com>
2017-05-08 14:24:34 -0700

    add second table for employees

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508210841) do
+ActiveRecord::Schema.define(version: 20170508211820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "Deapartments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.integer "division_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
